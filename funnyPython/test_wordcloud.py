97990e13d0bcd1a5e95b9399ccf50c02c912809c
comeCU <1404842608@qq.com>
2018-08-17 17:54:46 +0800

    funnyPython中添加了test_wordcloud文件

---
+++ funnyPython/test_wordcloud.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# 生成词云
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+
+filename = "***.txt"	# 文本
+with open(filename) as f:
+	mytext = f.read()
+
+# print(mytext)
+
+wordcloud = WordCloud().generate(mytext)
+
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")  # 隐藏坐标
+plt.show()
+
